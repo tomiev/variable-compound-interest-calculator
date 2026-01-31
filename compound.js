a7e72f378af3449cd2065bfe91b81aa8cd26d104
Todd Evans <evansmtodd@gmail.com>
2026-01-31 18:53:48 +1100

    handle varying stages

--- compound.js
+++ compound.js
@@ -1,33 +1,70 @@
 /**
- * Returns the total balance, contributions, and interest earned during
- * an investment period.
- * @param {int} principal - Starting balance
- * @param {int} payment - Regular payment amount
- * @param {int} paymentsPerYear - Number of regular payments made in a year
- * @param {int} rate - Annual interest rate (e.g. 10 = 10%)
- * @param {int} compoundsPerYear - Number of times interest is applied annually
- * @param {int} years - Number of years to use for this stage
+ * Returns the total balance, contributions, and interest earned in one year.
+ * @param {number} principal
+ * @param {int} regularContribution
+ * @param {int} contributionsPerYear
+ * @param {int} rate - annual interest rate
+ * @param {int} compoundsPerYear
+ * @param {number} - value of all contributions prior to this year
+ * @param {number} - value of all interest earned prior to this year
  */
-export function compound(principal, payment, paymentsPerYear, rate, compoundsPerYear, years) {
-  const decimalRate = rate / 100;
-  const principalGrowthFactor = (1 + decimalRate / compoundsPerYear) ** (compoundsPerYear * years);
-  const grownPrincipal = principal * principalGrowthFactor;
+export function calculateYear(
+  principal,
+  regularContribution,
+  contributionsPerYear,
+  rate,
+  compoundsPerYear,
+  previousInterest,
+  previousContributions
+) {
+  const r = rate / 100;
+  const grownPrincipal = principal * (1 + r / compoundsPerYear) ** compoundsPerYear;
 
-  const paymentGrowthFactor = (1 + decimalRate / compoundsPerYear) ** (compoundsPerYear / paymentsPerYear) - 1;
-  const grownPayments = (payment * ((1 + paymentGrowthFactor) ** (paymentsPerYear * years) - 1)) / paymentGrowthFactor;
+  const payments = regularContribution * contributionsPerYear;
+  const paymentGrowthFactor = (1 + r / compoundsPerYear) ** (compoundsPerYear / contributionsPerYear) - 1;
+  const grownPayments =
+    (regularContribution * ((1 + paymentGrowthFactor) ** contributionsPerYear - 1)) / paymentGrowthFactor;
 
   const balance = grownPrincipal + grownPayments;
-  const contributions = principal + payment * paymentsPerYear * years;
+  // If there are no previous contributions, we are in year 1 and the principal should be counted towards
+  // this year's contributions. Otherwise omit the principal.
+  const contributions = previousContributions === 0 ? principal + payments : payments;
+  const interest = balance - contributions - previousContributions - previousInterest;
 
   return {
     balance: balance,
     contributions: contributions,
-    interest: balance - contributions
+    interest: interest,
+    cumulativeInterest: interest + previousInterest,
+    cumulativeContributions: contributions + previousContributions
   };
 }
 
-// TODO pick a graphing solution and data structure.
-// To graph the results, I need to have the cumulative balance, contributions, and interest at the end of each year.
-// e.g. if 5 years, something like:
-// let data = [];
-// for (i = 0; i<5; i++) {data.push(compound(x, y, z))}
+/**
+ * Returns results from all years of the investment strategy.
+ * Output is an array of year data, e.g. [{balance, contributions, interest}, {balance, contributions, interest} ...];
+ * @param {int} principal
+ * @param {Array} stages - array of stage data
+ */
+export function getAllYearData(principal, stages) {
+  const yearlyData = [];
+
+  stages.forEach((s) => {
+    for (let i = 0; i < s.years; i++) {
+      const previousYear = yearlyData.at(-1);
+
+      const year = calculateYear(
+        previousYear?.balance ?? principal,
+        s.regularPayment,
+        s.paymentsPerYear,
+        s.rate,
+        s.compoundsPerYear,
+        previousYear?.cumulativeInterest ?? 0,
+        previousYear?.cumulativeContributions ?? 0
+      );
+
+      yearlyData.push(year);
+    }
+  });
+  return yearlyData;
+}
--- tests/test.js
+++ tests/test.js
@@ -1,42 +1,81 @@
 // Testing done with QUnit: https://qunitjs.com/
-// To run tests, start a local server and visit /tests/test.html in the browser
+// To run tests, visit /tests/test.html in the browser
 
-import { compound } from '../compound.js';
+import { calculateYear, getAllYearData } from '../compound.js';
 
-// Compound function tests
-QUnit.module('compound', function () {
-  QUnit.test('one year, no contributions', function (assert) {
-    const result = compound(100, 0, 12, 10, 1, 1);
+// Calculate year tests
+QUnit.module('calculateYear', function () {
+  QUnit.test('no contributions & annual compounding', function (assert) {
+    const result = calculateYear(100, 0, 12, 10, 1, 0, 0);
+    assert.equal(result.contributions.toFixed(2), '100.00');
+    assert.equal(result.interest.toFixed(2), '10.00');
     assert.equal(result.balance.toFixed(2), '110.00');
   });
 
-  QUnit.test('two years, no contributions', function (assert) {
-    const result = compound(100, 0, 12, 10, 1, 2);
-    assert.equal(result.balance.toFixed(2), '121.00');
+  QUnit.test('no contributions & monthly compounding', function (assert) {
+    const result = calculateYear(100, 0, 12, 10, 12, 0, 0);
+    assert.equal(result.contributions.toFixed(2), '100.00');
+    assert.equal(result.interest.toFixed(2), '10.47');
+    assert.equal(result.balance.toFixed(2), '110.47');
   });
 
-  QUnit.test('one year with monthly contributions & monthly compounding', function (assert) {
-    const result = compound(100, 10, 12, 10, 12, 1);
+  QUnit.test('monthly contributions & monthly compounding', function (assert) {
+    const result = calculateYear(100, 10, 12, 10, 12, 0, 0);
+    assert.equal(result.contributions.toFixed(2), '220.00');
+    assert.equal(result.interest.toFixed(2), '16.13');
     assert.equal(result.balance.toFixed(2), '236.13');
   });
 
-  QUnit.test('ten years with monthly contributions & monthly compounding', function (assert) {
-    const result = compound(100, 10, 12, 10, 12, 10);
-    assert.equal(result.balance.toFixed(2), '2319.15');
-  });
-
-  QUnit.test('one year with weekly contributions & annual compounding', function (assert) {
-    const result = compound(100, 10, 52, 10, 1, 1);
+  QUnit.test('weekly contributions & annual compounding', function (assert) {
+    const result = calculateYear(100, 10, 52, 10, 1, 0, 0);
+    assert.equal(result.contributions.toFixed(2), '620.00');
+    assert.equal(result.interest.toFixed(2), '35.09');
     assert.equal(result.balance.toFixed(2), '655.09');
   });
+});
 
-  QUnit.test('ten years with weekly contributions & annual compounding', function (assert) {
-    const result = compound(100, 10, 52, 10, 1, 10);
-    assert.equal(result.balance.toFixed(2), '8946.66');
-  });
+QUnit.module('getAllYearData', function () {
+  QUnit.test('three year strategy', function (assert) {
+    const principal = 100;
+    const stages = [
+      {
+        regularPayment: 10,
+        paymentsPerYear: 12,
+        rate: 10,
+        compoundsPerYear: 12,
+        years: 1
+      },
+      {
+        regularPayment: 20,
+        paymentsPerYear: 12,
+        rate: 10,
+        compoundsPerYear: 12,
+        years: 2
+      }
+    ];
 
-  QUnit.test('ten years with weekly contributions & monthly compounding', function (assert) {
-    const result = compound(100, 10, 52, 10, 12, 10);
-    assert.equal(result.balance.toFixed(2), '9175.72');
+    const years = getAllYearData(principal, stages);
+    assert.equal(years.length, 3);
+
+    const year1 = years[0];
+    assert.equal(year1.interest.toFixed(2), '16.13');
+    assert.equal(year1.contributions.toFixed(2), '220.00');
+    assert.equal(year1.balance.toFixed(2), '236.13');
+    assert.equal(year1.cumulativeInterest.toFixed(2), '16.13');
+    assert.equal(year1.cumulativeContributions.toFixed(2), '220.00');
+
+    const year2 = years[1];
+    assert.equal(year2.interest.toFixed(2), '36.04');
+    assert.equal(year2.contributions.toFixed(2), '240.00');
+    assert.equal(year2.balance.toFixed(2), '512.16');
+    assert.equal(year2.cumulativeInterest.toFixed(2), '52.16');
+    assert.equal(year2.cumulativeContributions.toFixed(2), '460.00');
+
+    const year3 = years[2];
+    assert.equal(year3.interest.toFixed(2), '64.94');
+    assert.equal(year3.contributions.toFixed(2), '240.00');
+    assert.equal(year3.balance.toFixed(2), '817.11');
+    assert.equal(year3.cumulativeInterest.toFixed(2), '117.11');
+    assert.equal(year3.cumulativeContributions.toFixed(2), '700.00');
   });
 });
